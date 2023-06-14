68be342bcef87442fe8fbe3f25e36df07d5ee806
Luigi Woodhose <luigiwoodhouse@gmail.com>
2023-06-13 21:57:33 -0500

    added spring initiliazer

--- src/main/java/com/starwar/movie/Main.java
+++ src/main/java/com/starwar/movie/Main.java
@@ -1,6 +1,7 @@
 package com.starwar.movie;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -8,7 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer implements CommandLineRunner {
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 
     @Override
